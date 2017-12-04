39d2ac613562edbaa843b97e5b3919e4c73abe3d
saike1119 <saike1119@gmail.com>
2017-12-05 08:47:52 +0900

    追記

--- chapter7/application/core/Request.php
+++ chapter7/application/core/Request.php
@@ -46,4 +46,33 @@ class Request
     {
         return $_SERVER['REQUEST_URI'];
     }
+
+    public function getBaseUrl()
+    {
+        $script_name = $_SERVER['SERVER_NAME'];
+
+        $request_uri = $this->getRequestUri();
+
+        if (0 === strpos($request_uri, $script_name)) {
+            return $script_name;
+        } else {
+            if (0 === strpos($request_uri, dirname($script_name))) {
+                return rtrim(($script_name), '/');
+            }
+        }
+        return '';
+    }
+
+    public function getPathInfo()
+    {
+        $base_url = $this->getBaseUrl();
+        $request_uri = $this->getRequestUri();
+
+        if (false !== ($pos = strpos($request_uri, '?'))) {
+            $request_uri = substr($request_uri, 0, $pos);
+        }
+        $path_info = (String)substr($request_uri, strlen($base_url));
+
+        return $path_info;
+    }
 }
