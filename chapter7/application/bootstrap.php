70cc08007f4a3093a85aee4549ec4959bfd87f8e
saike1119 <saike1119@gmail.com>
2017-12-05 23:28:40 +0900

    修正

--- chapter7/application/bootstrap.php
+++ chapter7/application/bootstrap.php
@@ -4,6 +4,6 @@
 require 'core/ClassLoader.php';
 
 $loader = new ClassLoader();
-$loader->registerDir(diename(__FILE__).'core');
-$loader->registerDir(diename(__FILE__).'models');
+$loader->registerDir(dirname(__FILE__).'core');
+$loader->registerDir(dirname(__FILE__).'models');
 $loader->register();
